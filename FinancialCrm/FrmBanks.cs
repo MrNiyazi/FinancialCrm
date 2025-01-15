186a241a77231a86213509ac348c2fef1b72a5fd
Berat Aktas <berataktas2834@gmail.com>
2025-01-15 23:44:33 +0300

    15.01.2025

--- FinancialCrm/FrmBanks.cs
+++ FinancialCrm/FrmBanks.cs
@@ -33,6 +33,20 @@ namespace FinancialCrm
 
 			//Banka Hareketleri
 
+			var bankProcess1 = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(1).FirstOrDefault();
+			lblBankProcess1.Text = bankProcess1.Description + " " + bankProcess1.Amoun + " " + bankProcess1.ProcessDate;
+
+			var bankProcess2 = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(2).Skip(1).FirstOrDefault();
+			lblBankProcess2.Text = bankProcess2.Description + " " + bankProcess2.Amoun + " " + bankProcess2.ProcessDate;
+
+			var bankProcess3 = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(3).Skip(2).FirstOrDefault();
+			lblBankProcess3.Text = bankProcess3.Description + " " + bankProcess3.Amoun + " " + bankProcess3.ProcessDate;
+
+			//var bankProcess4 = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(4).Skip(3).FirstOrDefault();
+			//lblBankProcess4.Text = bankProcess4.Description + " " + bankProcess4.Amoun + " " + bankProcess4.ProcessDate;
+
+			//var bankProcess5 = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(5).Skip(4).FirstOrDefault();
+			//lblBankProcess5.Text = bankProcess5.Description + " " + bankProcess5.Amoun + " " + bankProcess5.ProcessDate;
 		}
 	}
 }
